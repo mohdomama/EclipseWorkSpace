dc76abb3c0b7ede1ecb9447c4a2b26703b517fc7
Mohd Omama <mohd.omama@gmail.com>
2018-04-08 07:37:33 +0530

    Change Broker

--- MQTTApplication/src/main/java/com/softnerve/MQTTSubscriber.java
+++ MQTTApplication/src/main/java/com/softnerve/MQTTSubscriber.java
@@ -2,7 +2,7 @@ package com.softnerve;
 
 public class MQTTSubscriber {
     public static void main(String[] args) {
-        MQTTApp mqttApp1 = new MQTTApp("iot/mqtt", "tcp://iot.eclipse.org",
+        MQTTApp mqttApp1 = new MQTTApp("iot/mqtt", "tcp://broker.mqttdashboard.com",
                 "TestPublisher");
         
         mqttApp1.listen();
